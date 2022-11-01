86ae780864fa6355d424de7c47dc1fc20bc1c3ed
Stephen Pflug <stephen@pflug.us>
2022-11-01 09:12:31 -0500

    question 1 complete and tested)

--- src/Assessment.java
+++ src/Assessment.java
@@ -1,3 +1,12 @@
 public class Assessment {
 
+    public static int square(int num) {
+        return num * num;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(square(4));
+        System.out.println(square(12));
+    }
+
 }
