3474199c44abc98a375456a18e894c2a0e3954ce
Stephen Pflug <stephen@pflug.us>
2022-11-01 09:16:42 -0500

    rest of question 1 complete and tested

--- src/Assessment.java
+++ src/Assessment.java
@@ -4,9 +4,29 @@ public class Assessment {
         return num * num;
     }
 
+    public static int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    public static double sum(double num1, double num2) {
+        return num1 + num2;
+    }
+
+    public static double average(int[] nums) {
+        int total = 0;
+        for(int num : nums) {
+            total += num;
+        }
+        return (double) total / nums.length;
+    }
+
     public static void main(String[] args) {
-        System.out.println(square(4));
-        System.out.println(square(12));
+//        System.out.println(square(4));
+//        System.out.println(square(12));
+//        System.out.println(sum(4,5));
+//        System.out.println(sum(4.5, 5.5));
+//        int[] nums = {5, 10, 15};
+//        System.out.println(average(nums));
     }
 
 }
